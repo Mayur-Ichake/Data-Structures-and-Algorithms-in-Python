df40e90c1b68d0d00fe0e5622bd9354e7c8583cb
Mayur-Ichake <mayurichake4@gmail.com>
2026-02-17 22:12:05 +0530

    done

---
+++ Recursion/06_Print_GFG_n_imes.py
@@ -0,0 +1,21 @@
+'''Print GFG n times without the loop.
+
+Example:
+
+Input:
+5
+Output:
+GFG GFG GFG GFG GFG'''
+
+n = 5
+
+class Solution:
+    def printGfg(self, n):
+        # Code here
+        
+        if n == 0:
+            return 
+        
+        print("GFG", end=" ")
+        
+        self.printGfg(n-1)
