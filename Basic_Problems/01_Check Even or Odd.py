c16e48b5bf63cadb8c613d572d83de09fccdfa12
Mayur-Ichake <mayurichake4@gmail.com>
2026-02-24 14:28:13 +0530

    Done

---
+++ Basic_Problems/01_Check Even or Odd.py
@@ -0,0 +1,26 @@
+"""Given a number n, check whether it is even or odd. Return true for even and false for odd.
+
+Examples: 
+
+Input: n = 15
+Output: false
+Explanation: 15 % 2 = 1, so 15 is odd .
+
+Input: n = 44
+Output: true
+Explanation: 44 % 2 = 0, so 44 is even."""
+
+# def check_value(n):
+#     if (n & 1) == 0:
+#         return True
+#     else:
+#         return False
+    
+if __name__ == "__main__":
+    n = 19
+    if (n & 1) == 0:
+        print("true")
+    else:
+        print("false")
+
+
